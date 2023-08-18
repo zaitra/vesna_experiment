91fc4eac2298c1030f704a22b73c421e75ca2073
unknown <danielusmann@gmail.com>
2023-08-18 14:18:41 +0200

    modify and restructure tests

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -3,11 +3,18 @@ import EasyPySpin
 import typer
 from typer.testing import CliRunner
 
-
-def test_camera():
+def use_camera():
     cap = EasyPySpin.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FPS, 10)
     cap.release()
+
+app = typer.Typer()
+app.command()(use_camera)
+
+runner = CliRunner()
+
+def test_camera():
+    result = runner.invoke(app)
 
     # Check whether connection is proper based on warning that only occurs when the connection is bad
     assert "EasyPySpinWarning: AcquisitionFrameRate" not in result.stdout
